a43ba9c1d6f1668eeb1e6a37d0a628223151f9c3
THREEhundo <samuelbaik87@gmail.com>
2020-01-02 19:50:53 -0500

    game parameters built

--- js/game.js
+++ js/game.js
@@ -1,7 +1,45 @@
-console.log(`hi`);
 function computerPlay() {
   // randomly return rock, paper, scissors
-  let moves = ['🗿', '🧻', '✂️']
+  let moves = ['rock', 'paper', 'scissors']
   return moves[Math.floor(Math.random() * moves.length)]
 }
+
+function playRound(playerA, playerB) {
+  if (playerA == 'rock' && playerB == 'paper') {
+    return `You lose.
+    ${playerB} beats ${playerA}`
+  } else if (playerA == 'rock' && playerB == 'rock') {
+    return `Draw.
+    ${playerB} cancels out ${playerA}`
+  } else if (playerA == 'rock' && playerB == 'scissors') {
+    return `You win.
+    ${playerB} beats ${playerA}`
+  } else if (playerA == 'paper' && playerB == 'scissors') {
+    return `You lose.
+    ${playerB} beats ${playerA}`
+  } else if (playerA == 'paper' && playerB == 'paper') {
+    return `Draw.
+    ${playerB} cancels out ${playerA}`
+  } else if (playerA == 'paper' && playerB == 'rock') {
+    return `You win.
+    ${playerB} beats ${playerA}`
+  } else if (playerA == 'scissors' && playerB == 'rock') {
+    return `You lose.
+    ${playerB} beats ${playerA}`
+  } else if (playerA == 'scissors' && playerB == 'scissors') {
+    return `Draw.
+    ${playerB} cancels out ${playerA}`
+  } else if (playerA == 'scissors' && playerB == 'paper') {
+    return `You win.
+    ${playerB} beats ${playerA}`
+  }
+}
+
+function game() {
+
+}
+
 computerPlay()
+
+const playerSelection = 'paper'
+const computerSelection = computerPlay()
