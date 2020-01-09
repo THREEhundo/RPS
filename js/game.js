ae1a7cd9190dde569cc64035fb2ae9dd01cfd942
THREEhundo <samuelbaik87@gmail.com>
2020-01-08 22:27:10 -0500

    Refactored playRound(). Added counter to game(). Game testing done.

--- js/game.js
+++ js/game.js
@@ -1,45 +1,75 @@
-function computerPlay() {
-  // randomly return rock, paper, scissors
-  let moves = ['rock', 'paper', 'scissors']
-  return moves[Math.floor(Math.random() * moves.length)]
-}
-
-function playRound(playerA, playerB) {
-  if (playerA == 'rock' && playerB == 'paper') {
-    return `You lose.
-    ${playerB} beats ${playerA}`
-  } else if (playerA == 'rock' && playerB == 'rock') {
-    return `Draw.
-    ${playerB} cancels out ${playerA}`
-  } else if (playerA == 'rock' && playerB == 'scissors') {
-    return `You win.
-    ${playerB} beats ${playerA}`
-  } else if (playerA == 'paper' && playerB == 'scissors') {
-    return `You lose.
-    ${playerB} beats ${playerA}`
-  } else if (playerA == 'paper' && playerB == 'paper') {
-    return `Draw.
-    ${playerB} cancels out ${playerA}`
-  } else if (playerA == 'paper' && playerB == 'rock') {
-    return `You win.
-    ${playerB} beats ${playerA}`
-  } else if (playerA == 'scissors' && playerB == 'rock') {
-    return `You lose.
-    ${playerB} beats ${playerA}`
-  } else if (playerA == 'scissors' && playerB == 'scissors') {
-    return `Draw.
-    ${playerB} cancels out ${playerA}`
-  } else if (playerA == 'scissors' && playerB == 'paper') {
-    return `You win.
-    ${playerB} beats ${playerA}`
+document.addEventListener('DOMContentLoaded', function (event){
+  function computerPlay() {
+    // randomly return rock, paper, scissors
+    let moves = ['rock', 'paper', 'scissors']
+    return moves[Math.floor(Math.random() * moves.length)]
   }
-}
 
-function game() {
+  function playRound(playerA, playerB) {
+    // added var to keep count of rounds played
+    if (playerA == 'rock' && playerB == 'paper') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `You lose.`
+    } else if (playerA == 'rock' && playerB == 'rock') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `Draw.`
+    } else if (playerA == 'rock' && playerB == 'scissors') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `You win.`
+    } else if (playerA == 'paper' && playerB == 'scissors') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `You lose.`
+    } else if (playerA == 'paper' && playerB == 'paper') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `Draw.`
+    } else if (playerA == 'paper' && playerB == 'rock') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `You win.`
+    } else if (playerA == 'scissors' && playerB == 'rock') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `You lose.`
+    } else if (playerA == 'scissors' && playerB == 'scissors') {
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `Draw.`
+    } else if (playerA == 'scissors' && playerB == 'paper') {
+      // tabulate record
+      console.log(`playerA: ${playerA}, playerB: ${playerB}`);
+      return `You win.`
+    }
+  }
 
-}
+  function game() {
+    let playerTally = 0
+    let computerTally = 0
+    let tied = 0
+    for (var i = 0; i < 6; i++) {
+      let round = playRound(playerSelection, computerPlay())
+      console.log(round)
+      if (round == `You win.`) {
+        playerTally++
+        console.log(playerTally)
+      } else if (round == `You lose.`) {
+        computerTally++
+        console.log(computerTally)
+      } else if (round == `Draw.`) {
+        tied++
+        console.log(tied)
+      }
+    }
+    // compute totals and announce winner
+    if (playerTally > computerTally && playerTally > tied) {
+      console.log(`You Win`)
+    } else if (computerTally > playerTally && computerTally > tied) {
+      console.log(`Computer Wins`)
+    } else if (tied > computerTally && tied > playerTally) {
+      console.log(`Tie`)
+    }
 
-computerPlay()
+  }
 
-const playerSelection = 'paper'
-const computerSelection = computerPlay()
+  const playerSelection = 'paper'
+
+
+  computerPlay()
+  game()
+})
