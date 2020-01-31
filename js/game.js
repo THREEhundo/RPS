32c324e1c5630ac23c76b825ba523195c71ab052
THREEhundo <samuelbaik87@gmail.com>
2020-01-31 16:55:18 -0500

    Finished game logic.

--- js/game.js
+++ js/game.js
@@ -1,195 +1,177 @@
-// document.addEventListener('DOMContentLoaded', function (event){
-let playerScore = 0;
-let compScore = 0;
-let ties = 0;
-const currentPlayerScore = document.querySelector("#p-score");
-const currentCompScore = document.querySelector("#c-score");
-const playByPlay = document.querySelector("#play-by-play");
-const playerCurrentChoice = document.querySelector("#player-choice");
-const compCurrentChoice = document.querySelector("#comp-choice");
-const rock = document.querySelector("#r");
-const paper = document.querySelector("#p");
-const scissors = document.querySelector("#s");
-const compRock = document.querySelector("#cr");
-const compPaper = document.querySelector("#cp");
-const compScissors = document.querySelector("#cs");
-const shoot = document.querySelector("#shoot");
-const pBpRock = document.querySelector("#rock-text");
-const pBpPaper = document.querySelector("#paper-text");
-const pBpScissors = document.querySelector("#scissors-text");
-const pBpShoot = document.querySelector("#shoot-text");
-const ul = document.querySelector("ul");
-const header = document.querySelector("#comp-header");
+document.addEventListener('DOMContentLoaded', function(event) {
+  /*jshint esversion: 6 */
+  let playerScore = 0;
+  let compScore = 0;
+  const currentPlayerScore = document.querySelector("#p-score");
+  const currentCompScore = document.querySelector("#c-score");
+  const playByPlay = document.querySelector("#play-by-play");
+  const rock = document.querySelector("#r");
+  const paper = document.querySelector("#p");
+  const scissors = document.querySelector("#s");
+  const compRock = document.querySelector("#cr");
+  const compPaper = document.querySelector("#cp");
+  const compScissors = document.querySelector("#cs");
+  const pBpRock = document.querySelector("#rock-text");
+  const pBpPaper = document.querySelector("#paper-text");
+  const pBpScissors = document.querySelector("#scissors-text");
+  const ul = document.querySelector("ul");
 
-function stopAnimation(animation) {
-	animation.classList.remove("text-blur-out");
-}
+  noDisplay(ul);
+  loadingScreen();
+  addEventListeners();
 
-function startAnimation(animation) {
-	animation.classList.add("text-blur-out");
-}
+  function stopAnimation(animation) {
+    animation.classList.remove("text-blur-out");
+  }
 
-function hideSpans() {
-	compRock.style.visibility = "hidden";
-	compPaper.style.visibility = "hidden";
-	compScissors.style.visibility = "hidden";
-}
+  function startAnimation(animation) {
+    animation.classList.add("text-blur-out");
+  }
 
-function visible(el) {
-	el.style.visibility = "visible";
-}
+  function hideSpans() {
+    compRock.style.visibility = "hidden";
+    compPaper.style.visibility = "hidden";
+    compScissors.style.visibility = "hidden";
+  }
 
-function invisible(el) {
-	el.style.visibility = "hidden";
-}
+  function visible(el) {
+    el.style.visibility = "visible";
+  }
 
-function display(el) {
-	el.style.display = "";
-}
+  function display(el) {
+    el.style.display = "";
+  }
 
-function noDisplay(el) {
-	el.style.display = "none";
-}
+  function noDisplay(el) {
+    el.style.display = "none";
+  }
 
-function loadingScreen() {
-	setTimeout(function() {
-		stopAnimation(compRock);
-		stopAnimation(pBpRock);
-		setTimeout(startAnimation(compRock), 0);
-		setTimeout(startAnimation(pBpRock), 0);
-	}, 0);
-	setTimeout(function() {
-		stopAnimation(compPaper);
-		stopAnimation(pBpPaper);
-		setTimeout(startAnimation(compPaper), 0);
-		setTimeout(startAnimation(pBpPaper), 0);
-	}, 2500);
-	setTimeout(function() {
-		stopAnimation(compScissors);
-		stopAnimation(pBpScissors);
-		setTimeout(startAnimation(compScissors), 0);
-		setTimeout(startAnimation(pBpScissors), 0);
-		// header vis hidden
-	}, 5000);
-	setTimeout(hideSpans, 11000);
-	setTimeout(display, 11000, ul);
-}
+  function loadingScreen() {
+    setTimeout(function() {
+      stopAnimation(compRock);
+      stopAnimation(pBpRock);
+      setTimeout(startAnimation(compRock), 0);
+      setTimeout(startAnimation(pBpRock), 0);
+    }, 0);
+    setTimeout(function() {
+      stopAnimation(compPaper);
+      stopAnimation(pBpPaper);
+      setTimeout(startAnimation(compPaper), 0);
+      setTimeout(startAnimation(pBpPaper), 0);
+    }, 2500);
+    setTimeout(function() {
+      stopAnimation(compScissors);
+      stopAnimation(pBpScissors);
+      setTimeout(startAnimation(compScissors), 0);
+      setTimeout(startAnimation(pBpScissors), 0);
+    }, 5000);
+    setTimeout(hideSpans, 11000);
+    setTimeout(display, 11000, ul);
+  }
 
-// put eventlisteners in variables
-// create removeEventListener()
-// remove eventlisteners off while comp move is shown
-// add eventlisteners when comp move finishes.
+  function clickRock() {
+    playRound("r");
+  }
 
-// change addEventListener anonymous to variable
-function clickRock() {
-	playRound("r");
-}
+  function clickPaper() {
+    playRound("p");
+  }
 
-function clickPaper() {
-	playRound("p");
-}
+  function clickScissors() {
+    playRound("s");
+  }
+  // Allows player selection.
+  function addEventListeners() {
+    rock.addEventListener("click", clickRock);
+    paper.addEventListener("click", clickPaper);
+    scissors.addEventListener("click", clickScissors);
+  }
+  // Prevents players from forcing pick before animation ends.
+  function removeEventListeners() {
+    rock.removeEventListener("click", clickRock);
+    paper.removeEventListener("click", clickPaper);
+    scissors.removeEventListener("click", clickScissors);
+  }
 
-function clickScissors() {
-	playRound("s");
-}
-// player chooses move
-function addEventListeners() {
-	rock.addEventListener("click", clickRock);
-	paper.addEventListener("click", clickPaper);
-	scissors.addEventListener("click", clickScissors);
-}
+  function computerShoots() { // randomly chooses a move
+    const moves = ["r", "p", "s"];
+    return moves[Math.floor(Math.random() * 3)];
+  }
+  // outcomes
+  function win() {
+    playerScore++;
+    currentPlayerScore.innerHTML = playerScore;
+    playByPlay.innerHTML = "You Win";
+  }
 
-function removeEventListeners() {
-	rock.removeEventListener("click", clickRock);
-	paper.removeEventListener("click", clickPaper);
-	scissors.removeEventListener("click", clickScissors);
-}
+  function lose() {
+    compScore++;
+    currentCompScore.innerHTML = compScore;
+    playByPlay.innerHTML = "You Lose";
+  }
 
-function computerShoots() { // randomly chooses a move
-	const moves = ["r", "p", "s"];
-	return moves[Math.floor(Math.random() * 3)];
-}
-// outcomes
-function win() {
-	playerScore++;
-	currentPlayerScore.innerHTML = playerScore;
-	playByPlay.innerHTML = "You Win";
-}
+  function tie() {
+    playByPlay.innerHTML = "Tie";
+  }
 
-function lose() {
-	compScore++;
-	currentCompScore.innerHTML = compScore;
-	playByPlay.innerHTML = "You Lose";
-}
+  // Promise resolves in 1 second, Animation added for each computer pick.
+  function resolveAfterHalfSecond(picked) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(startAnimation(picked));
+      }, 500);
+    });
+  }
 
-function tie() {
-	ties++;
-	playByPlay.innerHTML = "Tie";
-}
 
-function showCompMove(pick) {
-	if (pick === "r") {
-		stopAnimation(compRock); // take off animation class
-		visible(compRock); // make span visible
-		setTimeout(function() {
-			startAnimation(compRock); // kill display for rock
-		}, 0);
-		// setTimeout(function() {
-		//   invisible(compRock)
-		// }, 3500)
-	} else if (pick === "p") {
-		stopAnimation(compPaper); // take off animation class
-		visible(compPaper);
-		setTimeout(function() {
-			startAnimation(compPaper); // kill display for rock
-		}, 0);
-		// setTimeout(function() {
-		//   invisible(compPaper)
-		// }, 3500)
-	} else if (pick === "s") {
-		stopAnimation(compScissors); // take off animation class
-		visible(compScissors);
-		setTimeout(function() {
-			startAnimation(compScissors); // kill display for rock
-		}, 0);
-		// setTimeout(function() {
-		//   invisible(compScissors)
-		// }, 3500)
-	}
-}
+  async function showCompMove(pick) {
+    if (pick === "r") {
+      visible(compRock); // make span visible
+      stopAnimation(compRock); // take off animation class
+      await resolveAfterHalfSecond(compRock); // add animation class
+    } else if (pick === "p") {
+      visible(compPaper);
+      stopAnimation(compPaper); // take off animation class
+      await resolveAfterHalfSecond(compPaper); // add animation class
+    } else if (pick === "s") {
+      visible(compScissors);
+      stopAnimation(compScissors); // take off animation class
+      await resolveAfterHalfSecond(compScissors); // add animation class
+    }
+  }
 
-function playRound(playerA) {
-	// Outcome conditions
-	removeEventListeners();
-	const compPick = computerShoots();
-	switch (playerA + compPick) {
-	case "rs":
-	case "pr":
-	case "sp":
-		console.log(compPick);
-		showCompMove(compPick);
-		win();
-		break;
-	case "rp":
-	case "ps":
-	case "sr":
-		console.log(compPick);
-		showCompMove(compPick);
-		lose();
-		break;
-	case "rr":
-	case "ss":
-	case "pp":
-		console.log(compPick);
-		showCompMove(compPick);
-		tie();
-		break;
-	}
-	setTimeout(function() {
-		addEventListeners();
-	}, 3500);
-}
-noDisplay(ul);
-loadingScreen();
-addEventListeners();
-// })
+  function playRound(playerA) {
+    // Outcome conditions
+    removeEventListeners();
+    const compPick = computerShoots();
+    switch (playerA + compPick) {
+      case "rs":
+      case "pr":
+      case "sp": {
+        console.log(compPick);
+        showCompMove(compPick);
+        win();
+        break;
+      }
+      case "rp":
+      case "ps":
+      case "sr": {
+        console.log(compPick);
+        showCompMove(compPick);
+        lose();
+        break;
+      }
+      case "rr":
+      case "ss":
+      case "pp": {
+        console.log(compPick);
+        showCompMove(compPick);
+        tie();
+        break;
+      }
+    }
+    setTimeout(function() {
+      addEventListeners();
+    }, 3500);
+  }
+});
